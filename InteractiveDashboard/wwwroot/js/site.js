6e87374616e3efbd54f0986331ba17ff0d9cd2cb
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-09-13 14:28:47 +0200

    commit 18

--- InteractiveDashboard/wwwroot/js/site.js
+++ InteractiveDashboard/wwwroot/js/site.js
@@ -1,8 +1,5 @@
 ﻿
 
-// ---------- CHARTS ----------
-
-// BAR CHART
 const barChartOptions = {
     series: [
         {
@@ -113,7 +110,6 @@ const barChart = new ApexCharts(
 );
 barChart.render();
 
-// AREA CHART
 const areaChartOptions = {
     series: [
         {
