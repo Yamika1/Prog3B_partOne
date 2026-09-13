e90d56df3334aa7ad95721408a8f51cddddcf544
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-09-13 11:57:13 +0200

    commit 2

---
+++ InteractiveDashboard/Controllers/SensorPayloadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveDashboard.Controllers
+{
+    public class SensorPayloadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
